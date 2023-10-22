f6c99f727616119e3e520f982f900413ffac3af8
thiva-k <kalyanasundaramthivaharan@gmail.com>
2023-10-22 15:17:26 +0530

    Fixed Filters

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar } from '../components/Navbar';
-import { Container, Grid, TextField, Button, Slider, Typography, Box } from '@mui/material';
+import { Container, Grid, TextField, Slider, Typography } from '@mui/material';
 import { RoomCard } from '../components/RoomCard';
 import { LoadingSkeleton } from '../components/LoadingSkeleton';
 import { collection, getDocs } from 'firebase/firestore';
@@ -11,28 +11,36 @@ import Footer from '../components/Footer';
 export default function Home({ setDarkMode }) {
   const currentDate = new Date().toISOString().split('T')[0]; // Get the current date in the "yyyy-MM-dd" format
 
-  const [startDate, setStartDate] = useState(currentDate); // Set the default "From" date to the current date
+  const [startDate, setStartDate] = useState(currentDate);
   const [endDate, setEndDate] = useState('');
-  const [guests, setGuests] = useState(1); // Set a default value for the number of guests
-  const [price, setPrice] = useState(1); // Set a default value for the price
+  const [guests, setGuests] = useState(1);
+  const [price, setPrice] = useState(1000);
+  const [filtersApplied, setFiltersApplied] = useState(true);
+  const [filteredData, setFilteredData] = useState([]);
 
   const fetchRooms = async () => {
     const roomsCollection = collection(db, 'rooms');
     const querySnapshot = await getDocs(roomsCollection);
     const data = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    console.log(data);
-    return data;
+
+    const newData = data.filter(
+      (room) => (!filtersApplied || (room.pricePerNight <= price && room.maxGuests >= guests))
+    );
+
+    return newData;
   };
 
-  const { data, isLoading } = useQuery('rooms', fetchRooms);
+  const { data = [], isLoading } = useQuery('rooms', fetchRooms);
 
-  const handleGuestsChange = (event, newValue) => {
-    setGuests(newValue);
-  };
-
-  const handlePriceChange = (event, newValue) => {
-    setPrice(newValue);
-  };
+  useEffect(() => {
+    if (filtersApplied) {
+      fetchRooms().then((newData) => {
+        setFilteredData(newData);
+      });
+    } else {
+      setFilteredData(data);
+    }
+  }, [filtersApplied, data, price, guests]);
 
   return isLoading ? (
     <LoadingSkeleton />
@@ -41,7 +49,6 @@ export default function Home({ setDarkMode }) {
       <Navbar setDarkMode={setDarkMode} />
       <main>
         <Container maxWidth={'lg'} sx={{ marginTop: 5 }}>
-          {/* Filter Section */}
           <div
             style={{
               display: 'flex',
@@ -59,7 +66,7 @@ export default function Home({ setDarkMode }) {
                 size="small"
                 value={startDate}
                 onChange={(e) => setStartDate(e.target.value)}
-                inputProps={{ min: currentDate }} // Set the minimum date to the current date
+                inputProps={{ min: currentDate }}
               />
             </div>
             <div style={{ flex: 1 }}>
@@ -70,39 +77,33 @@ export default function Home({ setDarkMode }) {
                 size="small"
                 value={endDate}
                 onChange={(e) => setEndDate(e.target.value)}
-                inputProps={{ min: startDate }} // Set the minimum date to the selected "From" date
+                inputProps={{ min: startDate }}
               />
             </div>
             <div style={{ flex: 1 }}>
               <Typography variant="body2">Number of Guests (1-10):</Typography>
               <Slider
                 value={guests}
-                onChange={handleGuestsChange}
                 min={1}
                 max={10}
                 valueLabelDisplay="auto"
+                onChange={(event, newValue) => setGuests(newValue)}
               />
             </div>
             <div style={{ flex: 1 }}>
-              <Typography variant="body2">Price $ ( 1- 1000):</Typography>
+              <Typography variant="body2">Price $ (1-1000):</Typography>
               <Slider
                 value={price}
-                onChange={handlePriceChange}
+                onChange={(event, newValue) => setPrice(newValue)}
                 min={1}
                 max={1000}
                 valueLabelDisplay="auto"
               />
             </div>
-            <div style={{ }}>
-              <Button variant="contained" color="primary">
-                Apply Filters
-              </Button>
-            </div>
           </div>
 
-          {/* Room Cards Section (Render your room cards here) */}
           <Grid container spacing={2}>
-            {data?.map((item) => (
+            {filteredData.map((item) => (
               <Grid key={item.id} item xs={12} md={4}>
                 <RoomCard room={item} />
               </Grid>
